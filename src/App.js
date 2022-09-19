6c09d97dfa9b938075397bc15b576940be912673
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-09-19 21:18:55 +0300

    adde

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const App = () => {
-  return <div className='bg-red-200'>react app</div>;
+  return (
+    <div className='container mx-auto'>
+      <div className='flex flex-col justify-start  space-y-4'>
+        <button className='btn btn-lg btn-accent'>Learn more</button>
+        <button className='btn btn-lg btn-accent'>Learn more</button>
+        <button className='btn btn-lg btn-accent'>Learn more</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
